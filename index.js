a09d6f5b51ad8bec9128bf1538add1ca942a192f
Pablo Cruz <pcruzp1400@gmail.com>
2024-03-25 13:37:41 -0600

    feat: crud complete

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-import previewData from "./js/modules/update.js"
-import validateForm from "./validacionform.js"
+import crearPost from './js/modules/crearPost.js'
+
+const filterFecha = (date, posts) => {
+  const fecha = document.querySelector('#filtro')
+  // recorrer posts y buscar elemento que coincida
+  const nuevoArray = posts.filter((post) => post.fecha == date)
+  return nuevoArray
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-  const currentId = new URLSearchParams(location.search).get('post');
-  currentId && previewData(currentId);
-  
-  const form = document.querySelector('#new-post')
-  form.addEventListener('submit', (event) => {
-    event.preventDefault()
-    if(validateForm()){const users = JSON.parse(localStorage.getItem('users')) ?? []
-    const formData = new FormData(form)
-    const entries = formData.entries()
-    const data = Object.fromEntries(entries)
-    data.hashtags = formData.getAll('hashtags')
-    data.id = users.length + 1
-    users.push(data)
-    const jsonString = JSON.stringify(users)
-    localStorage.setItem('users', jsonString)}
-    
+  const posts = JSON.parse(localStorage.getItem('users'))
+  crearPost(posts)
+
+  const calendario = document.querySelector('#filtro')
+  calendario.addEventListener('change', () => {
+    const date = calendario.value
+    const newArray = filterFecha(date, posts)
+    if (newArray.length == 0) {
+      document.getElementById('no-data').hidden = false
+      console.log('no data')
+    } else {
+      const principalCard = document.querySelector('#post-cards')
+      principalCard.innerHTML = ''
+      crearPost(newArray)
+    }
   })
-  console.log(typeof localStorage.getItem('users'))
 })
