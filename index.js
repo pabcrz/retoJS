fe6afc9f7436b51337ef97808f3232227491c39d
zuleima2611 <98924223+zuleima2611@users.noreply.github.com>
2024-03-25 17:40:29 -0600

    exportFuncion (#5)

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import crearPost from './js/modules/crearPost.js'
 import filtrarFecha from './js/modules/filtrarFecha.js'
 
+
 document.addEventListener('DOMContentLoaded', () => {
   const posts = JSON.parse(localStorage.getItem('users'))
+  
   crearPost(posts)
-  filtrarFecha(posts)
+ filtrarFecha(posts)
 })
