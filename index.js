5cd27a6a7f862e58b70fd6d24c3dbbd734569434
Pablo Cruz <pcruzp1400@gmail.com>
2024-03-25 19:49:00 -0600

    feat: reload button

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import crearPost from './js/modules/crearPost.js'
 import filtrarFecha from './js/modules/filtrarFecha.js'
 
-
 document.addEventListener('DOMContentLoaded', () => {
   const posts = JSON.parse(localStorage.getItem('users'))
-  
+
   crearPost(posts)
- filtrarFecha(posts)
+  filtrarFecha(posts)
 })
--- js/modules/filtrarFecha.js
+++ js/modules/filtrarFecha.js
@@ -1,4 +1,4 @@
-import crearPost from "./crearPost.js"
+import crearPost from './crearPost.js'
 
 const filterFecha = (date, posts) => {
   // const fecha = document.querySelector('#filtro')
@@ -9,24 +9,18 @@ const filterFecha = (date, posts) => {
 
 export default (posts) => {
   const calendario = document.querySelector('#filtro')
-  let noEncontro = 0
   calendario.addEventListener('change', () => {
+    const principalCard = document.querySelector('#post-cards')
+    principalCard.innerHTML = ''
     const date = calendario.value
     const newArray = filterFecha(date, posts)
+    const noData = document.getElementById('no-data')
     if (newArray.length == 0) {
-      if (noEncontro == 0) {
-        const noData = document.createElement('p')
-        noData.id = 'no-data'
-        noData.textContent = 'No se encontraron Post con la fecha seleccionada'
-        const postCards = document.getElementById('post-cards')
-        postCards.prepend(noData)
-        noEncontro++
-      }
+      noData.classList.remove('p-nodata')
       console.log('no hay posts')
     } else {
-      const principalCard = document.querySelector('#post-cards')
-      principalCard.innerHTML = ''
       crearPost(newArray)
+      noData.classList.add('p-nodata')
     }
   })
 }
